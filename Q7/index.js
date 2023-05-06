1e69dc0d69f824750dd954a56f87369aab1455aa
Lucas Mairon <120278386+LucasMairon@users.noreply.github.com>
2023-05-05 22:30:38 -0300

    Create index.js

---
+++ Q7/index.js
@@ -0,0 +1,10 @@
+
+const nome_informado = prompt("digite seu nome");
+const idade_informada = parseInt(prompt("digite sua idade"));
+const cidade_informada= prompt("digite o nome da sua cidade");
+let informacoes = {
+  nome: nome_informado,
+  idade: idade_informada,
+  cidade: cidade_informada,
+};
+console.log(informacoes);
